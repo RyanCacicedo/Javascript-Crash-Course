9b4b23b75497cad4c4131dee870990fd77d53651
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-04 15:51:29 -0500

    Loops practice question

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-for (let i = 1; i <= 20; i++) {
-    if (i%3===0 && i%5===0) {
-     console.log(`${i} -> Frontend simplified`)
-    }
-  else if (i%3===0) {
-    console.log(`${i} -> frontend`)
-   }
-  else if (i%5===0) {
-    console.log(`${i} -> simplified`)
-   }
- else {
-    console.log(`${i} -> ${i}`)
-   }
-   
-}
 
+
+const str = `Frontend Simplified`
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+}
