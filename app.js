3369e3991437c0c092bce6a7684dd51320d3a19f
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-04 14:16:14 -0500

    javascript practice

---
+++ app.js
@@ -0,0 +1,30 @@
+let celsius = 10
+let fahrenheit = celsius * 1.8 +32
+
+console.log(fahrenheit)
+
+let subscribed = true
+let loggedIn = true
+
+if (subscribed === true) {
+    console.log(`show the video`)
+}
+else if (loggedIn === true) {
+    console.log(`tell the user to upgrade their subscription`)
+}
+else {
+    console.log(`tell user to log into account`)
+}
+
+let cash = 150
+let price = 200
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${cash-price} dollars change`)
+}
+else if (cash===price) {
+    console.log(`you paid the exact amount, have a nice day!`)
+}
+else [
+    console.log(`not enough money - you still owe ${price - cash} dollars`)
+]
