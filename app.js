8de33df0811276c815bf29153e42fc490795bdd1
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-05 16:09:28 -0500

    convert celsius to fahrenheit using a function

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-function welcomePersonToClass(name) {
-    console.log(`Welcome to class, ${name}`)
+function convertCelsiusToFahrenheit (celsius) {
+    return celsius * 1.8 + 32
 }
 
-welcomePersonToClass(`Ryan`)
-welcomePersonToClass(`Nick`)
-welcomePersonToClass(`Brian`)
-
-function sumOfTwoNumbers(num1, num2) {
-    return num1 + num2
-}
-
-console.log(sumOfTwoNumbers(10,10))
+console.log(convertCelsiusToFahrenheit(0))
+console.log(convertCelsiusToFahrenheit(10))
+console.log(convertCelsiusToFahrenheit(30))
