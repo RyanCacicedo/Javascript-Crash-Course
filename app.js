3db23a0e377251be6dbbf025b63db1a6872e6e63
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-11 14:45:52 -0500

    DOM practice

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-let users = [
-  {
-    username: `Ryan`,
-    email: `ryancacicedo@gmail.com`,
-    password: `test123`,
-    subscriptionStatus: `VIP`,
-    discordId: `Ryan Cac#001`,
-    lessonsCompleted: [0, 1],
-  },
-];
+// First way of accessing an element
+console.log(document.querySelector(`#title`))
 
-function login(email, password) {
-  for (let i = 0; i < users.length; ++i) {
-    if (users[i].email === email) {
-      if (users[i].password === password) {
-        console.log(`log the user in - the details are correct`);
-      } else {
-        console.log(`password is incorrect - try again`);
-      }
-      return;
-    }
-  }
-  console.log(`could not find an email that matches`);
-}
-
-login(`ryancacicedo@gmail.com`, `test123`);
-
-function register(user) {
-  users.push(user)
-}
-
-register({
-  username: `Ryan`,
-  email: `ryancacicedo@gmail.com`,
-  password: `Test123`,
-  subscriptionStatus: `VIP`,
-  discordId: `Ryan cac#001`,
-  lessonsCompleted: [0, 1]
-});
-
-console.log(users);
+// Second way of accessing an element
+console.log(document.getElementsById(`title`))
