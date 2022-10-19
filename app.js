be7f12e3f42ca14bc3a47c64bc7e1fbd5369c43f
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-19 16:30:29 -0400

    Create promises

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-// fetch("https://jsonplaceholder.typicode.com/users/1")
+const statusRef = document.querySelector(`.status`)
 
-const emailRef = document.querySelector(".email");
-console.log(emailRef);
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("VIP")
+        }, 2000)
+    })
+}
 
 // 1. Then
-//fetch("https://jsonplaceholder.typicode.com/users/1")
-//  .then((response) => {
-//    return response.json();
-//  })
-//  .then((data) => {
-//    console.log(data);
-//    emailRef.innerHTML = data.email;
-//  });
-
+// getSubscriptionStatus().then(response => console.log(response))
 
 // 2. Async/Await
 async function main() {
-   const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await response.json()
-    emailRef.innerHTML = data.email
+    const status = (await getSubscriptionStatus())
+    statusRef.innerHTML = status
 }
 
-main()
+main();
