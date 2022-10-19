5d999e4896fc54751137105115193bfe74efa4b2
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-19 16:54:58 -0400

    Using promises & if functions

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const statusRef = document.querySelector(`.status`)
+const videoRef = document.querySelector(`.video`)
 
 function getSubscriptionStatus() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("VIP")
-        }, 2000)
+            resolve(undefined);
+        }, 2000);
     })
 }
 
-// 1. Then
-// getSubscriptionStatus().then(response => console.log(response))
+function getVideo(subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+       if (subscriptionStatus === "VIP") {
+        resolve("show video")
+       }
+       else if (subscriptionStatus === "FREE") {
+        resolve("show trailer")
+       }
+       else {
+        reject("no video")
+       }
+    })
+}
 
-// 2. Async/Await
 async function main() {
-    const status = (await getSubscriptionStatus())
-    statusRef.innerHTML = status
+    const status = await getSubscriptionStatus();
+    statusRef.innerHTML = status;
+    try {
+        console.log(await getVideo(status))
+    }
+    catch (e) {
+        console.log(e)
+        videoRef.innerHTML = e
+    }
 }
 
 main();
