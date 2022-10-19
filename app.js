c67d8c92a02e8a8d17385efb748f62900ff2f119
RyanCacicedo <Ryancacicedo@gmail.com>
2022-10-19 15:04:52 -0500

    updates

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-// How to change the HTML of any element with DOM
-document.querySelector(`#title`).innerHTML = `Ryan`
+// fetch("https://jsonplaceholder.typicode.com/users/1")
 
-// Change the CSS
+const emailRef = document.querySelector(".email");
+console.log(emailRef);
 
-document.querySelector(`#title`).style.color = `red`
+// 1. Then
+//fetch("https://jsonplaceholder.typicode.com/users/1")
+//  .then((response) => {
+//    return response.json();
+//  })
+//  .then((data) => {
+//    console.log(data);
+//    emailRef.innerHTML = data.email;
+//  });
+
+
+// 2. Async/Await
+async function main() {
+    await fetch("https://jsonplaceholder.typicode.com/users/1")
+}
+
+main()
